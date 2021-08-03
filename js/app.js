c2e2905c4dedb18eeb2223ede90d9038e0e8535b
sidnet1964 <sidnet@mail.ru>
2021-08-03 08:40:52 +0300

    last day added

--- js/app.js
+++ js/app.js
@@ -6,16 +6,54 @@ function updateExchangeRates(updateRates){
     document.getElementById('eur-buy-rate').textContent = updateRates.eur.buyRate;
     document.getElementById('eur-sell-rate').textContent = updateRates.eur.sellRate;
 }
+function handleSubmit(evt){
+    evt.preventDefault();
 
-const update = {
-    date: '21 июля',
-    usd: {
-        buyRate: '73,00',
-        sellRate: '75,00',
-    },
-    eur: {
-        buyRate: '86,00',
-        sellRate: '88,00',
+    // const phone = phoneInputEl.value.trim();
+    // const email = emailInputEl.value.trim();
+    //
+    // const data = {
+    //     phone,
+    //     email,
+    // };
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:9999/api/hw13');
+    // xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = handleSuccess;
+    xhr.onerror = handleError;
+    // xhr.send((JSON.stringify(data)));
+    xhr.send();
+}
+function handleSuccess(evt){
+    if (evt.target.status !== 200) {
+        // TODO: handle error
+        return;
     }
-};
-updateExchangeRates(update);
+    const data = JSON.parse(evt.target.responseText);
+    updateExchangeRates(data);
+    // TODO: work with data
+}
+
+function handleError(evt){
+    // TODO: handle error
+}
+
+// const formEl = document.getElementById('card-form');
+// formEl.addEventListener('submit', handleSubmit);
+const formEl = document.getElementById('date');
+formEl.addEventListener('loadstart', handleSubmit);
+
+// handleSubmit();
+// const update = {
+//     date: '21 июля',
+//     usd: {
+//         buyRate: '73,00',
+//         sellRate: '75,00',
+//     },
+//     eur: {
+//         buyRate: '86,00',
+//         sellRate: '88,00',
+//     }
+// };
+// updateExchangeRates(update);
